3abb7ac5a95b02ce2a4b8cb0147c91daaf5796ae
Sumanpatra0209 <sumanpatra91360@gmail.com>
2023-10-19 13:19:37 +0000

    react routing practice issue

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,29 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const BlogItem = props => {
+  const {BlogItemDetails} = props
+  const {id, imageUrl, topic, title, avatarUrl, author} = BlogItemDetails
+
+  return (
+    <li className="blogItem">
+      <Link className="blogItemLink" to={`/blogs/${id}`}>
+        <div className="blogItemContainer">
+          <img className="blogItemImage" src={imageUrl} alt={`item${id}`} />
+          <div className="blogItemInfo">
+            <p className="blogItemTopic">{topic}</p>
+            <h1 className="blogItemTitle">{title}</h1>
+            <div className="authorInfo">
+              <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
+              <p className="authorName">{author}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default BlogItem
